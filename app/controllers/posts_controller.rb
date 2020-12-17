bbacddab4fddd2c3e505ba6a6ad3a671cb552a41
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-17 11:55:42 -0800

    commit tree e004db502b4b6f1e74501a5ee3f9d2c6d1f9dfc2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,13 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    
     if @post.valid?
+     
       @post.save
       redirect_to post_path(@post)
     else
+      
       render :new
     end
   end
